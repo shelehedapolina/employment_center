b78e55469f4d79ef71a44fc2014568ccfc5dcd81
Polina Sheleheda <sheleheda.polina1123@vu.cdu.edu.ua>
2026-05-07 18:59:26 +0300

    fix remote db connection

--- app/config/db.js
+++ app/config/db.js
@@ -1,7 +1,15 @@
 const { Pool } = require('pg');
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set. Define it in your environment (e.g. postgres://user:pass@host:5432/dbname).');
+}
+
+const LOCAL_HOSTS = /@(localhost|127\.0\.0\.1|db|postgres)(:|\/)/i;
+const isRemote = !LOCAL_HOSTS.test(process.env.DATABASE_URL);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: isRemote ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
